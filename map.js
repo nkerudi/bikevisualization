9706433e58401e94970b575888b346631e1a87ca
nkerudi <nikithakerudi@gmail.com>
2026-05-15 10:24:03 -0700

    added mapbox gl js

---
+++ map.js
@@ -0,0 +1,4 @@
+// Import Mapbox as an ESM module
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+//check that mapbox gl js is loaded 
+console.log("Mapbox GL JS Loaded:", mapboxgl);
